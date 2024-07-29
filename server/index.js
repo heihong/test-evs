3ae2527cd19ba96995cf2213a2a0892a93fcc825
htraing <traingheihong>
2024-07-29 22:41:57 +0200

    feat: change return route when add a item

--- server/index.js
+++ server/index.js
@@ -9,10 +9,12 @@ app.use(cors());
 
 
 let book1 = {
+    id:'book1',
     title:'angular'
 }
 
 let book2 = {
+    id:'book2',
     title:'html'
 }
 
@@ -22,9 +24,14 @@ app.get('/api/items', (req, res) => {
   res.json(items);
 });
 
+const randomId = function(length = 6) {
+    return Math.random().toString(36).substring(2, length+2);
+};
+
+
 app.post('/api/items', (req, res) => {
   const newItem = req.body;
-  items.push(newItem);
+  items.push({id: randomId, title: newItem.title});
   res.status(201).json(newItem);
 });
 
